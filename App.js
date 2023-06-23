973ff898e2bfb8a1bea5e8d41a975b121e200474
seonghun <tjdgns8439@naver.com>
2023-06-23 23:30:53 +0900

    Update App.js
    - 초기화면은 방 리스트
    - 등록 버튼 설정

--- App.js
+++ App.js
@@ -1,42 +1,95 @@
 import User from './screens/User'; 
-import { StatusBar, StyleSheet, Text, Button,View, ScrollView } from 'react-native';
+import Enroll from './screens/Enroll'; 
+import React, { useEffect, useState } from 'react';
+import { StatusBar, StyleSheet, Text, Button, View, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+const HomeScreen = ({ navigation }) => {
+  const [data, setData] = useState([]);
 
-const HomeScreen = ({navigation}) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://192.168.219.104:8080/room/list');
+        const json = await response.json();
+
+        // 객체를 배열로 변환
+        const dataArray = Object.values(json);
+
+        setData(dataArray);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (data.length === 0) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{flex: 2,
-      alignItems: 'center',
-      justifyContent: 'center'}}>
-      <Button
+    <View style={style}>
+      {data.map((room) => (
+        <View style={style} key={room.roomId}>
+          <Text>roomId: {room.roomId} hostID: {room.hostId.userId}</Text>
+          <Text>{room.start} -- {room.destination}</Text>
+        </View>
+      ))}
+      {/* <Button
         title="Go to User"
-        onPress={ () => navigation.navigate('User')}
-      />
+        onPress={() => navigation.navigate('User')}
+      /> */}
       <Button
-        title="Room enroll"
-        onPress={ () => navigation.navigate('User')}
+        title="Go to enroll"
+        onPress={() => navigation.navigate('Enroll')}
       />
-
     </View>
-  )
-}
+  );
+};
+
 const Stack = createStackNavigator();
 
-export default function App(){
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="User" component={User}/>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="User" component={User} />
+        <Stack.Screen name="Enroll" component={Enroll} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   btn: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop:Platform.OS === 'android' ? StatusBar.currentHeight : 0, }
-})
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
+
+const style = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor : '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    item: {
+      flex: 1,
+      alignSelf: 'stretch',
+      margin: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderBottomWidth: 1,
+      borderBottomColor: '#eee'
+    }
+  });
